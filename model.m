077113b10777eb5c9c7993d8ec7d48a6cc3f0a13
Payton Thomas <paytonco@gmail.com>
2021-07-01 20:57:27 -0600

    added model file

---
+++ model.m
@@ -0,0 +1,24 @@
+h = [1,1,0.1,1,1,0.1]; 
+H = diag(h); %rate constants
+
+S = [-1,-1,1,0,0,0;
+    1,1,-1,0,0,0;
+    1,0,-1,0,1,0;
+    0,0,0,-1,-1,1;
+    0,0,0,1,1,-1;
+    0,1,0,1,0,-1]'; %Stoichiometric matrix
+
+X0 = [1;
+    50;
+    0;
+    1;
+    50;
+    0]; %Initial state
+
+F = zeros(6,1);
+F(5,1) = 1; %Target condition matrix (F^T*x_t=xp)
+
+xp = [40]; %target state (F^T*x_t=xp)
+
+tmax = 100 %total simulation time
+
