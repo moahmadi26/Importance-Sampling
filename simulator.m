2adbfdd39442ff54224c7b551ceea4932854e245
Mo Ahmadi <mo@VPN-157-3.vpn.usf.edu>
2021-07-06 15:13:32 -0400

    simulator main body updated

--- simulator.m
+++ simulator.m
@@ -1,7 +1,8 @@
 k = [1;
     1;
     0.1;
-    1;1;
+    1;
+    1;
     0.1]; %rate constants
 
 S = [-1,-1,1,0,0,0;
@@ -19,67 +20,80 @@ X0 = [1;
     0]; %Initial state
 
 F = zeros(6,1);
+
 F(5,1) = 1; %Target condition matrix (F^T*x_t=xp)
 
 xp = [40]; %target state (F^T*x_t=xp)
 
 tmax = 100; %total simulation time
 
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
-N = 1
+%simulation runs
+N = 1000;
 
-mn = 0
+%accumulator
+mn = 0;
+
+
 for i = 1:N
-   t = 0 
-   delta_t = tmax - t
-   x = X0
-   w = 1
    
-   h = calculatePropensity(x, k)
+   t = 0;
+   delta_t = tmax - t;
+   x = X0;
+   w = 1;
+   
+   %evaluate all hj, h0 would be sum(h)
+   h = calculatePropensity(x, k);
+
    while t<tmax
        if xp == F'*x
-          mn = mn + w
-          break
+          mn = mn + w;
+          break;
        end
-       d = presimulationCheck(x,F,S,k,delta_t,xp,X0)
-       flag = 0
-       for j = 1:length(d)
-           if d(j)<=0
-               flag = 1
+       
+       %evaluate all di
+       
+       d = presimulationCheck(x,F,S,k,delta_t,xp,X0);
+       
+       %set h_tilde based on d. if all di>0 don't adjust model
+       flag = 0;
+       for v = 1:length(d)
+           if d(v)<=0
+               flag = 1;
            end
        end
-       if flag == 0
        
-            h_tilde = calculatePropensity(x, k)
+       if flag == 0
+            h_tilde = calculatePropensity(x, k);
       
        elseif flag == 1
-       
-            h_tilde = calculatePredilection(x,k,S,F,delta_t,xp)
+            h_tilde = calculatePredilection(x,k,S,F,delta_t,xp);
        end
        
-       r1 = rand
-       r2 = rand
+       r1 = rand;
+       r2 = rand;
        
-       tau = -(1.0/sum(h_tilde)) * log (r1)
+       tau = -(1.0/sum(h_tilde)) * log(r1);
        
-       temp_sum = 0 
-       it = 1
+       %randomly selecting one of reactions. Probablity distribution of 
+       %selection is based on each reaction's relative propensity
+       temp_sum = 0;
+       j = 1;
        
        while temp_sum <= r2*sum(h_tilde)
-           temp_sum = temp_sum + h_tilde(it)
-           it = it + 1
+           temp_sum = temp_sum + h_tilde(j);
+           j = j + 1;
        end
        
-       it = it - 1
-       w = w * (h(it)/h_tilde(it)) * exp((sum(h_tilde) - sum(h))*tau)
-       t = t + tau
-       delta_t = tmax - t
-       x = x + S(:,it)
-       h = calculatePropensity(x,k)
+       j = j - 1;
+       w = w * (h(j)/h_tilde(j)) * exp((sum(h_tilde) - sum(h))*tau);
+       t = t + tau;
+       delta_t = tmax - t;
+       x = x + S(:,j);
+       h = calculatePropensity(x,k);
        
        
-               
    end
 end
-p = mn/N
-disp(p)
+p = mn/N;
