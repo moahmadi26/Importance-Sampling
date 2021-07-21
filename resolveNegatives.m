7debc6b46f57fda5b596dc13d735fede42b42fee
Mo Ahmadi <mo@USF-Gold-Wifi-226-124-245.laptops.usf.edu>
2021-07-21 15:15:16 -0400

    resolveNegarives.m bug fixed

--- resolveNegatives.m
+++ resolveNegatives.m
@@ -1,7 +1,11 @@
 function alphaOut = resolveNegatives(alphaIn, method)
     if method == "A"
         
-        alphaIn = alphaIn + abs(min(alphaIn)) + 0.01;
+        for it = 1:length(alphaIn)
+           if alphaIn(it)<0
+               alphaIn = alphaIn + abs(min(alphaIn)) + 0.01;
+           end
+        end
         for it = 1:length(alphaIn)
            if (isnan(alphaIn(it))) | (alphaIn(it)<0)
                alphaIn(it) = 1;
